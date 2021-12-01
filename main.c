fde96c6687eca5dc759d299019ae432132d462ca
iamtahirkhan6 <48375147+iamtahirkhan6@users.noreply.github.com>
2021-12-01 06:55:46 +0530

    Initial Code

---
+++ main.c
@@ -0,0 +1,13 @@
+/*
+ * C program for budget planner
+ * The planner maintains details such as income, expenses.
+ * 
+ */
+
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+    /* TODO */
+}
