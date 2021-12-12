4c98f753d6ce2f73cd654878fc82e7c594ba546c
iamtahirkhan6 <48375147+iamtahirkhan6@users.noreply.github.com>
2021-12-12 15:21:00 +0530

    BudgetPlanner append by Ruthra

---
+++ BudgetPlanner.cpp
@@ -0,0 +1,33 @@
+class BudgetPlanner {       
+  public:             
+    float TotalBalance=0; 
+    //pass the vector containing the balance of all the months 
+    void UpdateTotalBalance(vector<string> &v){
+        for (i=0;i<12;i++)
+         {
+             TotalBalance = TotalBalance + v[i] ;
+         }
+        return TotalBalance;
+    };
+    
+    void ReadDocument(vector<string> &v){
+	  ifstream file;
+	  file.open("budget.txt");
+	  string line;
+	  while(getline(file, line)){
+		v.push_back(line);
+	  }
+	file.close();
+    }
+
+    void WriteDocument(vector<string> &v){
+        ofstream file;
+	    file.open("budget.txt");
+	    for(int i=0;i<v.size();++i){
+		   file<<v[i]<<endl;
+	}
+	file.close(); 
+
+    };
+    
+};
