4e8718d7e36290516da41bcbcbbb4e0f3a5ae0eb
iamtahirkhan6 <48375147+iamtahirkhan6@users.noreply.github.com>
2021-12-12 14:57:13 +0530

    Entry class Skeleton

---
+++ Entry.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+class Entry
+
+{
+    public:
+        int source;
+        int amount;
+
+    float GetAmount()
+    {
+    
+    }
+
+    string GetSource()
+    {
+    
+    }
+
+    double BudgetImpact()
+    {
+    
+    }
+
+    void ModifyAmount()
+    {
+    
+    }
+};
